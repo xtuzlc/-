1af693bcd7bfc50e87699aa03844cd054ea17c11
xtuzlc <zlcxtu41857@gmail.com>
2020-06-09 21:07:08 +0800

    react学习

---
+++ javascript/ReactDemo/demo01/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const Component = React.Component
+class App extends Component{
+    render(){
+        return(
+            <div>
+                hello ZLC
+                <h1>hhe</h1>
+            </div>           
+           
+        )
+    }
+}
+export default App
---
+++ javascript/ReactDemo/demo01/src/Xiaojiejie.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+const Component = React.Component
+//Fragment
+class Xiaojiejie extends Component{
+    render(){
+        return(
+            <Fragment>
+            <div><input></input>+<input></input>=<input></input></div>
+            <button>计算</button>
+            <div><input /> <button> 增加服务 </button></div>
+            <ul>
+                <li>头部按摩</li>
+                <li>精油推背</li>
+            </ul>
+            </Fragment>
+
+        )
+      
+
+    }
+}
+export default Xiaojiejie
+ 
